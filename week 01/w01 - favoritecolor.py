9a7416895e304d2ed90a43beafa05fe112426a9c
Bruno Belarmino <42091923+belarminobrunoz@users.noreply.github.com>
2021-05-03 22:47:50 -0300

    Add files and folders to the main structure

---
+++ week 01/w01 - favoritecolor.py
@@ -0,0 +1,11 @@
+#This program was made by Bruno Belarmino
+print("***Hello! I'm a program call favColor!***\n\n")
+
+name = input("What is your name?\n")
+
+print("Nice to meet you "+ name+"!\n")
+
+favcolor = input(name+", what's your favorite color?\n")
+
+print("Nice "+name+"! "+favcolor+" is a beautiful color! My favorite color is yellow :D")
+
---
+++ week 03/w03.py
@@ -0,0 +1,15 @@
+print("\n\n#### Welcome to Python Mad Libs Game ####")
+print("\nTo play, please enter the following information:")
+print("\n")
+adjective = input("adjective: ")
+animal = input("animal: ")
+verb1 = input("verb: ")
+exclamation = input("exclamation: ").capitalize()
+verb2 = input("verb: ")
+verb3 = input("verb: ")
+
+print("\n")
+print("Thank you for these informations :D \n")
+print("Your story is:\n\n")
+print("The other day, I was really in trouble. It all started when I saw a very "+adjective+" "+animal+" "+verb1+" down the hallway. "+'"'+exclamation+'!"' +
+      " I yelled. But all I could think to do was to "+verb2+" over and over. Miraculously, that caused it to stop, but not before it tried to "+verb3+" right in front of my family.")
