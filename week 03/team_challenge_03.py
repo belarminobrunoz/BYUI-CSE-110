2b4f8676ee12d4cb3210dea363eda11964dcb20f
Bruno Belarmino <42091923+belarminobrunoz@users.noreply.github.com>
2021-05-14 23:35:31 -0300

    Update team_challenge_03.py

--- week 03/team_challenge_03.py
+++ week 03/team_challenge_03.py
@@ -1,22 +1,30 @@
-# Import math Library
 import math
-
-sqr_length = float(input("What is the length of a side of the square? "))
-sqr_area = sqr_length*sqr_length
-print(f"The area of the square is: {sqr_area}")
-
-print("\n***************************************************************\n")
-
-rec_length  = float(input("What is the length of rectangle? "))
-rec_width = float(input("What is the width of the rectangle? "))
-rec_area = rec_length*rec_width
-print(f"The area of the rectangle is: {rec_area}")
-
-print("\n***************************************************************\n")
-
-rad_circle = float(input("5 What is the radius of the circle? "))
-cir_area = math.pi * (rad_circle**2)
-print(f"The area of the circle is: {cir_area}")
+length_square = float(input('What is the length of a side of the square in centimeters? '))
+square_centimeters = length_square ** 2
+square_meters = square_centimeters / 10000
+print(f'The area of the square in square centimeters is : {square_centimeters}')
+print(f'The area of square in square meters is: {square_meters}')
+length_rectangle = float(input('What is the length of rectangle in centimeters? '))
+width_rectangle = float(input('What is the width of rectangle in centimeters? '))
+rectangle_centimeters = length_rectangle * width_rectangle
+rectangle_meters = rectangle_centimeters / 10000
+print(f'The area of the rectangle in square centimeters is: {rectangle_centimeters}')
+print(f'The area of rectangle in square meters is: {rectangle_meters}')
+radius = float(input('What is the radius of the circle in centimeters? '))
+circle_centimeters = math.pi * (radius ** 2)
+circle_meters = circle_centimeters / 10000
+print(f'The area of the circle in square centimeters is: {circle_centimeters}')
+print(f'The area of the circle in square meters is: {circle_meters}')
+()
+# value = float(input('Enter a value: '))
+# sqr_area = value ** 2
+# cir_area = math.pi * (value ** 2)
+# cube_volume = value ** 3
+# sphere_volume = 4/3 * (math.pi * (value ** 3))
+# print(f'The area of square is: {sqr_area}')
+# print(f'The are of circle is: {cir_area}')
+# print(f'The volume of cube is: {cube_volume}')
+# print(f'The volume of sphere is: {sphere_volume}')
 
 
 # The stretch challenges for this activity are:
@@ -24,5 +32,11 @@ print(f"The area of the circle is: {cir_area}")
 # OOOOOK! -  Instead of using 3.14 for your value of Pi, see if you can find and use the built-in value of Pi included in the python "math" module. Hint, you might try searching on the internet for something like, "python how to get the value of pi."
 
 # Prompt the user for a single length value, then compute and display the areas of a square with that length of side, a circle with that radius, and then the volumes of a cube with that side and a sphere with that radius, all from the same value from the user.
+
+# receives a single length value, then compute and display 
+# -> the areas of a square with that length of side, 
+# -> a circle with that radius
+# -> the volumes of a cube with that side 
+# -> a sphere with that radius
 
 # For each of the three areas in the core requirements, change the prompt for the user to ask for the value in centimeters. Then, display the resulting area in both square centimeters and square meters. Keep in mind that a centimeter is 1/100 of a meter, and a square centimeter is 1/10,000 of a square meter.
