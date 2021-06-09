7f795ea7cea12a924250072b2e341cb12ff5f00e
Bruno Belarmino <42091923+belarminobrunoz@users.noreply.github.com>
2021-06-09 02:01:30 -0300

    criação de script para remover fundo de imagens com chroma key

--- week 07/w07_milestone.py
+++ week 07/w07_milestone.py
@@ -3,7 +3,38 @@
 from PIL import Image
 
 # This line opens the image and loads it into a variable called "image_original"
-image_original = Image.open("C:\Users\bruno\Documents\GitHub\BYUI-CSE-110\week 07\cse110_images\beach.jpg")
+image_original = Image.open("cse110_images\cat.jpg")
+image_beach = Image.open("cse110_images\praia.jpg")
 
 # This line attempts to open a new window to display the image.
+# image_original.show()
+
+width, height = image_original.size
+
+print(f"height {height} | width {width}")
+
+
+pixels_original = image_original.load()
+pixels_beach = image_beach.load()
+
+r, g, b = pixels_original[0, 0]
+print(f"chroma color: {r} - {g} - {b}")
+
+# cor verde chroma: 76 - 244 - 24
+
+for index1 in range(0,width):
+    for index2 in range(0,height):   
+        # if index1 % 100 == 0: 
+        #     print(f"Loop: {index1} - {index2} <---")
+
+        r, g, b = pixels_original[index1, index2]
+        # print(f"{r} - {g} - {b}")
+
+        if r < 120 and g > 210 and b < 60:
+            # Mudar o pixel
+            r, g, b = pixels_beach[index1, index2]
+            pixels_original[index1, index2] = (r, g, b)
+
+#Salvar o arquivo
+image_original.save("the_file_goes_here.jpg")
 image_original.show()
---
+++ week 07/w08_loops.py
@@ -0,0 +1,23 @@
+
+# #Aqui estou criando umma variavel chamada 'name' e fazendo o loop em cima desse array
+# for name in ['bruno', 'fran']:
+#     print(name)
+
+
+# #Aqui estou criando um range onde coloco o numero inicial, neste caso 0 e a quantidade de numeros neste range, que no caso é 3
+# for index in range(0,3):
+#     print(index)
+
+# #Looping with a condition
+# names = ['bruno', 'fran', 'caio']
+# index = 0
+# while index < len(names):
+#     print(names[index])
+#     #change the condition
+#     index = index +1
+
+
+names = ['bruno', 'fran', 'caio']
+
+for name in names:
+    print("My name is: " + name)
